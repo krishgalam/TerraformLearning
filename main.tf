b1b2817a05d9539adcebac1c95fad872018ba093
krishgalam <krishgalam@gmail.com>
2021-06-09 16:49:01 +0530

    commited all files

--- main.tf
+++ main.tf
@@ -1,7 +1,37 @@
-resource "azurerm_resource_group" "ARG" {
-    name = "FirstRG"
-    location = ""
-    tags = {
-      "Environment" = "Test"
-    }
+resource "azurerm_resource_group" "Test" {
+  name     = "demo-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "Test" {
+  name                = "demo-appserviceplan-hari"
+  location            = azurerm_resource_group.Test.location
+  resource_group_name = azurerm_resource_group.Test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "Test" {
+  name                = "demo-app-service-hari"
+  location            = azurerm_resource_group.Test.location
+  resource_group_name = azurerm_resource_group.Test.name
+  app_service_plan_id = azurerm_app_service_plan.Test.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "azurerm" { 
-    version = "2.20.1"
     subscription_id = "36fa8384-fff9-42f4-afcf-52fce4d6736e"
     client_id = ""
     tenant_id = ""
