7c3134b4d186578d63d4ff0dbee2026ecb232f58
krishgalam <krishgalam@gmail.com>
2021-06-12 12:31:39 +0530

    added new files

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-resource "azurerm_resource_group" "Test" {
-  name     = var.resource-group-name
-  location = var.location
-}
-
-resource "azurerm_app_service_plan" "Test" {
-  name                = var.app-service-plan-name
-  location            = azurerm_resource_group.Test.location
-  resource_group_name = azurerm_resource_group.Test.name
-
-  sku {
-    tier = var.sku.tier
-    size = var.sku.size
-  }
-}
-
-resource "azurerm_app_service" "Test" {
-  name                = var.app-service-name
-  location            = azurerm_resource_group.Test.location
-  resource_group_name = azurerm_resource_group.Test.name
-  app_service_plan_id = azurerm_app_service_plan.Test.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
+module "Module" {
+    source = "./Module"
 }
---
+++ module/ResourceGroup/variables.tf
@@ -0,0 +1,42 @@
+variable "resource-group-name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "app-service-plan-name" {
+  description = "App service plan Name"
+  type        = string
+}
+
+variable "app-service-name" {
+  type        = string
+}
+
+variable "location" {
+  description = "resource location"
+  type        = string
+}
+
+# variable "sku-tier" {
+#   type = string
+   
+# }
+# variable "sku-size" {
+#   type = string
+   
+# }
+
+variable "sku" {
+    type = object({
+        tier = string
+        size = string
+    })
+}
+
+# variable "user_information" {
+#   type = object({
+#     name    = string
+#     address = string
+#   })
+#   sensitive = true
+# }
